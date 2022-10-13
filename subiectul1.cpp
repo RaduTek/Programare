9d1f82f79fb93abf62ddb63060b117fe2ebf46b2
RaduTek <radutux13@gmail.com>
2022-10-13 20:06:01 +0300

    Subiecte

--- subiectul1.cpp
+++ subiectul1.cpp
@@ -1,30 +1,3 @@
-/*
-Subiectul nr.1
-Fişierul date.in  conține, pe prima linie,  un număr natural  n (n<100), iar pe fiecare din următoarele n linii este scris câte un cuvânt format  din cel mult 20de caractere.
-Se consideră subprogramul S1 care primește prin intermediul singurului său parametru s un cuvânt și verifică dacă acesta este palindrom. Un cuvânt care poate fi citit de la stânga la dreapta și de la dreapta la stânga reprezintă același cuvânt este un palindrom, de exemplu: “rar”, “potop”.
-
-Cerinţe:
-a)	Scrieţi definiţia completă a subprogramului S1;
-b)	Scrieţi un program principal Pascal/C/C++ care, folosind apeluri utile ale subprogramului S1  citește numărul n și cele n cuvinte, apoi determină toate cuvintele de tip palindrom și le afișează, câte unul pe o linie a ecranului. Dacă niciunul dintre cele n cuvinte citite nu este de tip palindrom, atunci programul va afișa pe ecran mesajul NU EXISTA.
-
-Exemplu.  Dacă fişierul  date.in are conţinutul alăturat, programul va afişa pe ecran: 
-
-    Fisier:
-    6
-    cojoc
-    iarna
-    ana
-    primavara
-    potop
-    toamna
-
-    Iesire:
-    cojoc
-    ana
-    potop
-
-*/
-
 #include <iostream>
 #include <cstring>
 
--- subiectul2.cpp
+++ subiectul2.cpp
@@ -1,23 +1,3 @@
-/*
-Subiectul nr.2
-Fişierul numere.in  conține, pe prima linie,  un număr natural  n (2<n<100) iar  pe linia a doua n numere cu cel mult 9 cifre fiecare. 
-Se consideră subprogramul sub care primește prin intermediul parametrului x un număr natural și returnează valoarea 1 dacă x aparține șirului lui Fibonacci și 0 în caz contrar.
-Cerinţe:
-a)	Scrieţi definiţia completă a subprogramului sub;
-b)	Scrieţi un program Pascal/C/C++ care, folosind apeluri utile ale subprogramului sub  citește numărul n și cele n numere, apoi determină toate numerele care aparțin șirului lui Fibonacci și le afișează, separate prin câte un spațiu, pe ecran. Dacă niciunul dintre cele n numere nu aparțin șirului lui Fibonacci, atunci programul va afișa pe ecran mesajul NU EXISTA.
-Şirul lui Fibonacci, “legea creşterilor organice”, se va considera aşa cum a fost definit de către Leonardo Fibonacci:1,1,2,3,5,8…
-
-Exemplu.  Dacă fişierul  numere.in  are conţinutul alăturat, programul va afişa pe ecran: 
-
-    Fisier:
-    6
-    5 20 21 8 13 15 
-
-    Iesire:
-    5 21 8 13
-
-*/
-
 #include <iostream>
 
 using namespace std;
